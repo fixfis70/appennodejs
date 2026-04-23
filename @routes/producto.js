790ca846a48790a0b264936d24a6eb632304bf3d
SENATI1 <carcamo@anyip.pe>
2026-04-23 12:57:16 -0500

    init spa

--- @routes/producto.js
+++ @routes/producto.js
@@ -3,37 +3,37 @@ const router = express.Router();
 const {pool} = require('../@config/db');
 
 router.get('/', async (req, res) => {
-    try{
+    try {
         const [rows] = await pool.query('SELECT * FROM productos ORDER BY id DESC');
         res.json(rows);
-    } catch(err){
+    } catch (err) {
         console.error(err);
         res.status(404).json({error: err.message});
     }
 })
 router.get('/:id', async (req, res) => {
-    try{
+    try {
         const id = req.params.id;
         const [rows] = await pool.query("select * FROM productos WHERE id = ?", [id]);
 
-        if (rows.length <1) {
+        if (rows.length < 1) {
             return res.status(404).json({success: false});
         }
 
         res.json(rows);
-    } catch(err){
+    } catch (err) {
         console.error(err);
         res.status(404).json({error: err.message});
     }
 })
 
 router.post('/', async (req, res) => {
-    const {idmarca, nombre, precio, garantia, descripcion,fechacompra} = req.body;
+    const {idmarca, nombre, precio, garantia, descripcion, fechacompra} = req.body;
 
-    if (!idmarca || !nombre || !precio || !garantia || !fechacompra){
+    if (!idmarca || !nombre || !precio || !garantia || !fechacompra) {
         return res.status(400).json({success: false});
     }
-    const [query] = await pool.query(`insert into productos (idmarca, nombre, precio, garantia, descripcion,fechacompra) value (?,?,?,?,?,?)`,[idmarca, nombre, precio, garantia, descripcion,fechacompra]);
+    const [query] = await pool.query(`insert into productos (idmarca, nombre, precio, garantia, descripcion, fechacompra) value (?, ?, ?, ?, ?, ?)`, [idmarca, nombre, precio, garantia, descripcion, fechacompra]);
 
     return res.status(201).json({success: true, data: query.insertId});
 })
@@ -41,13 +41,23 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     const {id} = req.params;
-    const {idmarca, nombre, precio, garantia, descripcion,fechacompra} = req.body;
+    const {idmarca, nombre, precio, garantia, descripcion, fechacompra} = req.body;
 
-    if (!idmarca || !nombre || !precio || !garantia || !fechacompra){
+    if (!idmarca || !nombre || !precio || !garantia || !fechacompra) {
         return res.status(400).json({success: false});
     }
 
-    const [result] = await pool.query("update productos set idmarca = ? and nombre = ? and precio = ? and garantia = ? and descripcion= ? and fechacompra = ? where id = ?",[idmarca, nombre, precio, garantia, descripcion,fechacompra,id]);
+    const [result] = await pool.query(
+        `UPDATE productos
+         SET idmarca = ?,
+             nombre = ?,
+             precio = ?,
+             garantia = ?,
+             descripcion = ?,
+             fechacompra = ?
+         WHERE id = ?`,
+        [idmarca, nombre, precio, garantia, descripcion, fechacompra, id]
+    );
     if (result.affectedRows) {
         return res.status(201).json({success: true, data: "Se actualizó el dato"});
     } else {
