1d84ccc0825a155920dc82c8b09c70400c1830b2
SENATI1 <carcamo@anyip.pe>
2026-04-23 09:52:10 -0500

    bro

--- @routes/producto.js
+++ @routes/producto.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../@config/db')
 
-
+router.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+    })
+})
 
 module.exports = router;
